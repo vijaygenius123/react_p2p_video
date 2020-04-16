1731558aa36879ebfa173bea8adda65f3fdf0282
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-16 23:30:21 +0530

    Added SocketIO

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ export default function Header() {
             alignLinks="right"
             brand={<a className="brand-logo" href="#">P2P Video</a>}
             id="mobile-nav"
+            centerLogo
             menuIcon={<Icon>menu</Icon>}
             options={{
                 draggable: true,
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,29 +1,63 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { Row, Col } from 'react-materialize'
-
+import Peer from "simple-peer";
+import io from 'socket.io-client'
 
 
 export default function Video() {
+    const [myId, setMyId] = useState("");
     const [stream, setStream] = useState();
 
 
+    const userVideo = useRef();
+    const partnerVideo = useRef();
+    const socket = useRef();
+
+
     useEffect(() => {
+        socket.current = io.connect("/");
         navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
             setStream(stream)
             if (userVideo.current) {
                 userVideo.current.srcObject = stream;
             }
-        }
-        )
-    })
+        })
 
-    const userVideo = useRef();
+        socket.current.on("myId", (id) => {
+            setMyId(id);
+        })
+
+        socket.current.on("hey", (data) => {
+
+        })
+    }, [])
+
+
 
     let UserVideo;
     if (stream) {
         UserVideo = (
             <video playsInline muted ref={userVideo} autoPlay className="user-video" />
         );
+    }
+
+    const callPeer = (id) => {
+        const peer = new Peer({
+            initiator: true,
+            trickle: false,
+            stream: stream
+        });
+
+        peer.on("signal", data => {
+            socket.current.emit("callUser", { userToCall: id, signalData: data, from: myId })
+        })
+
+        peer.on("stream", stream => {
+            if (partnerVideo.current) {
+                partnerVideo.current.srcObject = stream;
+            }
+        })
+
     }
 
     return (
